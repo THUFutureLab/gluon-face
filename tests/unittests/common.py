7aaa37a11a7d526bcb009c809a66c0a6f56f5bf9
PistonYang <pistonyang@gmail.com>
2019-06-19 15:20:47 +0800

    finish test basic block.

--- tests/unittests/common.py
+++ tests/unittests/common.py
@@ -1,4 +1,44 @@
 # -*- coding: utf-8 -*-
 # @Author  : PistonYang(pistonyang@gmail.com)
 
-from contextlib import contextmanager
+
+import mxnet as mx
+import functools
+
+
+def try_gpu(gpu_id=0):
+    """Try execute on gpu, if not fallback to cpu"""
+
+    def test_helper(orig_test):
+        @functools.wraps(orig_test)
+        def test_wapper(*args, **kwargs):
+            try:
+                a = mx.nd.zeros((1,), ctx=mx.gpu(gpu_id))
+                ctx = mx.gpu(gpu_id)
+            except Exception:
+                ctx = mx.cpu()
+            with ctx:
+                orig_test(*args, **kwargs)
+
+        return test_wapper
+
+    return test_helper
+
+
+def try_cpu(cpu_id=0):
+    """Try execute on gpu, if not fallback to cpu"""
+
+    def test_helper(orig_test):
+        @functools.wraps(orig_test)
+        def test_wapper(*args, **kwargs):
+            try:
+                a = mx.nd.zeros((1,), ctx=mx.cpu(cpu_id))
+                ctx = mx.gpu(cpu_id)
+            except Exception:
+                ctx = mx.cpu(0)
+            with ctx:
+                orig_test(*args, **kwargs)
+
+        return test_wapper
+
+    return test_helper
--- tests/unittests/test_basic_block.py
+++ tests/unittests/test_basic_block.py
@@ -1,4 +1,38 @@
 # -*- coding: utf-8 -*-
 # @Author  : PistonYang(pistonyang@gmail.com)
 
+import pytest
+import mxnet as mx
+from mxnet import nd
+from mxnet.gluon import nn
+from gluonfr.nn.basic_blocks import FrBase
+from tests.unittests.common import try_gpu
 
+
+class Model(FrBase):
+    def __init__(self, embedding_size=1, weight_norm=False, feature_norm=False,
+                 need_cls_layer=True):
+        super(Model, self).__init__(1, embedding_size, weight_norm, feature_norm, need_cls_layer)
+        self.features = nn.HybridSequential()
+        self.features.add(
+            nn.Conv2D(16, 3, 1, 1, use_bias=False),
+            nn.BatchNorm(),
+            nn.Dense(embedding_size, use_bias=False),
+            nn.BatchNorm(center=False, scale=False),
+        )
+
+
+@pytest.fixture(params=[[16, False, False, False],
+                        [16, True, True, False],
+                        [16, True, True, True]])
+def get_model(request):
+    return Model(*request.param)
+
+
+@try_gpu()
+def test_model(get_model):
+    ctx = mx.context.current_context()
+    x = nd.random.normal(shape=(1, 3, 112, 112), ctx=ctx)
+    model = get_model
+    model.initialize(ctx=ctx)
+    model(x)
