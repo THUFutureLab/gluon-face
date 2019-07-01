29cae7f39ce9422109b0d037ffd01e09c9dd9c62
Maybe <haoxintongpku@gmail.com>
2019-07-01 17:12:48 +0800

    API change of RingLoss, initializer is not required, just use a scalar instead

--- gluonfr/loss.py
+++ gluonfr/loss.py
@@ -67,69 +67,6 @@ def _reshape_like(F, x, y):
     return x.reshape(y.shape) if F is nd.ndarray else F.reshape_like(x, y)
 
 
-# SoftmaxCrossEntropyLoss.__doc__ = \
-#     r"""Computes the softmax cross entropy loss. (alias: SoftmaxCELoss)
-#
-#     If `sparse_label` is `True` (default), label should contain integer
-#     category indicators:
-#
-#     .. math::
-#
-#         \DeclareMathOperator{softmax}{softmax}
-#
-#         p = \softmax({pred})
-#
-#         L = -\sum_i \log p_{i,{label}_i}
-#
-#     `label`'s shape should be `pred`'s shape with the `axis` dimension removed.
-#     i.e. for `pred` with shape (1,2,3,4) and `axis = 2`, `label`'s shape should
-#     be (1,2,4).
-#
-#     If `sparse_label` is `False`, `label` should contain probability distribution
-#     and `label`'s shape should be the same with `pred`:
-#
-#     .. math::
-#
-#         p = \softmax({pred})
-#
-#         L = -\sum_i \sum_j {label}_j \log p_{ij}
-#
-#     Parameters
-#     ----------
-#     axis : int, default -1
-#         The axis to sum over when computing softmax and entropy.
-#     sparse_label : bool, default True
-#         Whether label is an integer array instead of probability distribution.
-#     from_logits : bool, default False
-#         Whether input is a log probability (usually from log_softmax) instead
-#         of unnormalized numbers.
-#     weight : float or None
-#         Global scalar weight for loss.
-#     batch_axis : int, default 0
-#         The axis that represents mini-batch.
-#
-#
-#     Inputs:
-#         - **pred**: the prediction tensor, where the `batch_axis` dimension
-#           ranges over batch size and `axis` dimension ranges over the number
-#           of classes.
-#         - **label**: the truth tensor. When `sparse_label` is True, `label`'s
-#           shape should be `pred`'s shape with the `axis` dimension removed.
-#           i.e. for `pred` with shape (1,2,3,4) and `axis = 2`, `label`'s shape
-#           should be (1,2,4) and values should be integers between 0 and 2. If
-#           `sparse_label` is False, `label`'s shape must be the same as `pred`
-#           and values should be floats in the range `[0, 1]`.
-#         - **sample_weight**: element-wise weighting tensor. Must be broadcastable
-#           to the same shape as label. For example, if label has shape (64, 10)
-#           and you want to weigh each sample in the batch separately,
-#           sample_weight should have shape (64, 1).
-#
-#     Outputs:
-#         - **loss**: loss tensor with shape (batch_size,). Dimenions other than
-#           batch_axis are averaged out.
-#     """
-
-
 class L2Softmax(SoftmaxCrossEntropyLoss):
     r"""L2Softmax from
     `"L2-constrained Softmax Loss for Discriminative Face Verification"
@@ -369,8 +306,10 @@ class RingLoss(SoftmaxCrossEntropyLoss):
     Parameters
     ----------
 
-    lamda: float
+    lamda: float.
         The loss weight enforcing a trade-off between the softmax loss and ring loss.
+    r_init: float.
+        The initial value of Hyper Parameter R.
 
 
     Outputs:
@@ -379,11 +318,11 @@ class RingLoss(SoftmaxCrossEntropyLoss):
 
     """
 
-    def __init__(self, lamda, weight_initializer=None, dtype='float32', **kwargs):
+    def __init__(self, lamda, r_init=1.0, dtype='float32', **kwargs):
         super().__init__(**kwargs)
 
         self._lamda = lamda
-        self.R = self.params.get('R', shape=(1,), init=weight_initializer,
+        self.R = self.params.get('R', shape=(1,), init=init.Constant(r_init),
                                  dtype=dtype, allow_deferred_init=True)
 
     def hybrid_forward(self, F, pred, label, embedding, R, sample_weight=None):
